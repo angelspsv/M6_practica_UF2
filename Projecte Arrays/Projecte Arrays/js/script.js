4a360eaabf5b49aed5c7c7f31c303efdec2e853b
angel_ivanov <angelspsv@gmail.com>
2023-12-23 12:51:36 +0100

    beginning exercise 2

--- Projecte Arrays/Projecte Arrays/js/script.js
+++ Projecte Arrays/Projecte Arrays/js/script.js
@@ -1,5 +1,5 @@
 // ////////////////////////////
-// //      Exercici 0        //
+// //      Part 0            //
 // ////////////////////////////
 
 let dadesPokemon;
@@ -76,7 +76,7 @@ function mostraDades() {
 fetch("js/data/pokemon.json")
 .then((response) => response.json())
 .then((data) => {
-	dadesPokemon = data.pokemon;		
+	dades = data.pokemon;		
 	
 	//console.log(dades)
 	//console.log(dades[0].name);
@@ -87,7 +87,7 @@ fetch("js/data/pokemon.json")
 fetch("js/data/municipis.json")
 .then((response) => response.json())
 .then((data) => {
-	dadesMunicipis = data.elements;		
+	dades = data.elements;		
 	
 	//console.log(dades)
 	//console.log(dades[0].municipi_nom)
@@ -98,7 +98,7 @@ fetch("js/data/municipis.json")
 fetch("js/data/movies.json")
 .then((response) => response.json())
 .then((data) => {
-	dadesMovies = data.movies;		
+	dades = data.movies;		
 	
 	//console.log(dades)
 	//console.log(dades[0].title)
@@ -109,7 +109,7 @@ fetch("js/data/movies.json")
 fetch("js/data/earthMeteorites.json")
 .then((response) => response.json())
 .then((data) => {
-	dadesMeteorits = data;		
+	dades = data;		
 	
 	//console.log(dades)
 	//console.log(dades[0].name)
@@ -119,7 +119,7 @@ fetch("js/data/earthMeteorites.json")
 */
 
 // ////////////////////////////
-// //      Exercici 1        //
+// //      Part 1            //
 // ////////////////////////////
 
 //funció de proba per l'opció reload
@@ -136,8 +136,9 @@ function ReloadWindow(){
 //funció que ordena mots de forma ascendent
 function OrdenaASC(){
 	let entrada = document.getElementById("espai_per_text").value;
-	document.getElementById("mostra_colors_asc").innerHTML = TextToArray(entrada);
+	document.getElementById("mostra_mots_ordenats").innerHTML = TextToArray(entrada);
 }
+
 
 //funció que rep un text i retorna un array de mots
 //la funció comprova quin és el separador dins del text, espai en blanc o la coma
@@ -154,6 +155,7 @@ function TextToArray(text){
 	return myArr.sort();
 }
 
+//funció que recorre el text buscant coma
 function CheckComa(text){
 	for(let i=0; i<text.length; i++){
 		if(text.charAt(i) == ","){
@@ -162,3 +164,58 @@ function CheckComa(text){
 	}
 	return false;
 }
+
+
+//funció que ordena descendentment els mots que rep
+function OrdenaDSC(){
+	let entrada2 = document.getElementById("espai_per_text").value;
+	let arr_tmp = TextToArray(entrada2);
+	document.getElementById("mostra_mots_ordenats").innerHTML = arr_tmp.reverse();
+}
+
+
+//funció que cerca un fragment dins d'un array
+function CercaPokemon(){
+	let nom_poke = document.getElementById("espai_per_nom_pokemon").value;
+	//treballar amb el nom del pokemon i l'array de pokemons que ja tinc
+	//en l'array de pokemons el nom te aquesta estructura: 'Aaaaaaaa'
+	let lletra = nom_poke.charAt(0).toUpperCase();
+	let temp = (nom_poke.toLowerCase()).slice(1);
+	let nom_cercat = lletra + temp;
+	document.getElementById("resposta_poke").innerHTML = CercaEnArray(nom_cercat);
+}
+
+//funció que cerca el nom del pokemon dins de l'array que ja teniem
+function CercaEnArray(text){
+	for(let i=0; i<dadesPokemon.length; i++){
+		if(text == dadesPokemon[i].name){
+			return "Aquest pokemon existeix!";
+		}
+	}
+	return "Aquest pokemon no existeix!";
+}
+
+
+//funció que administra un array
+//demana un número i el afegeix a l'array
+//fa el càlcul de la mitja i el retorna
+function MitjanaDelArray(){
+	let number = document.getElementById("num_entrat").value;
+	let arr_nums = [2, 5];
+	arr_nums.push(parseInt(number));
+	let mitja = (Suma(arr_nums)/(arr_nums.length)).toFixed(2);
+	document.getElementById("la_mitja").innerHTML = `La mitja de tots el números de l'array és: ${mitja}`;
+}
+
+function Suma(arr){
+	let suma = 0;
+	for(let i=0; i<arr.length; i++){
+		suma += arr[i];
+	}
+	return suma;
+}
+
+
+//Programa que rep un String i el retorna ordenat ascendentment o descendentment depenent si l'usuari
+//prem el botó ASC o el botó DSC
+let entrada3 = document.getElementById("cadena").value;
